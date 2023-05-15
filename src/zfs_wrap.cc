57645f4064e43d98f9e0142675fd18a18a3078f3
zhangxs <cheungssan@163.com>
2023-05-15 14:33:23 +0800

    init zfs

--- src/zfs_wrap.cc
+++ src/zfs_wrap.cc
@@ -4,6 +4,7 @@
 #include "node_external_reference.h"
 #include "node_internals.h"
 #include "util-inl.h"
+#include "uv.h"
 
 namespace node {
 
@@ -59,15 +60,6 @@ void Zfs::Initialize(Local<Object> target,
       ->Set(
           env->context(), str, t->GetFunction(env->context()).ToLocalChecked())
       .Check();
-
-  // // 申请一个字符串
-  // Local<String> str = FIXED_ONE_BYTE_STRING(env->isolate(), "console");
-  // // 设置函数类名
-  // t->SetClassName(str);
-  // // 导出函数，target即exports
-  // target->Set(env->context(),
-  //             str,
-  //             t->GetFunction(env->context()).ToLocalChecked()).Check();
 }
 
 void Zfs::New(const v8::FunctionCallbackInfo<v8::Value>& args) {
@@ -77,9 +69,13 @@ void Zfs::New(const v8::FunctionCallbackInfo<v8::Value>& args) {
 
 Zfs::Zfs(Environment* env) {}
 
-bool init = false;
-
 void Zfs::Init(const FunctionCallbackInfo<Value>& args) {
+  
+  char exepath[FILENAME_MAX+1];
+  size_t exepath_size = sizeof(exepath)-1;
+
+  uv_exepath(exepath, &exepath_size);
+  printf("exepath %s\n", exepath);
   const char* zipfile = "/home/cloud/lib.zip";
   unzFile uf = unzOpen64(zipfile);
   if (uf == NULL) {
@@ -111,16 +107,29 @@ void Zfs::Init(const FunctionCallbackInfo<Value>& args) {
     }
 
     printf(" file zip : %s\n", filename_inzip);
+    int len = strlen(filename_inzip);
+    if (filename_inzip[len - 1] == '/') {
+      err = unzGoToNextFile(uf);
+      if (err != UNZ_OK) {
+        printf("error %d with zipfile in unzGoToNextFile\n", err);
+        break;
+      }
+      continue;
+    }
+
     unzFile file;
-    err = unzOpenCurrentFile(&file);
-    char* content = new char[file_info.size_file_extra + 1];
-    content[file_info.size_file_extra] = 0x0;
-    unzReadCurrentFile(file, content, file_info.size_file_extra);
+    err = unzOpenCurrentFile(uf);
+    char* content = new char[file_info.uncompressed_size + 1];
+    content[file_info.uncompressed_size] = 0x0;
+    unzReadCurrentFile(uf, content, file_info.uncompressed_size);
+    printf("unzReadCurrentFile 2\n");
     std::string path(":/");
     path += filename_inzip;
 
+    printf("path %s\n", path.c_str());
+    content[file_info.uncompressed_size] = 0x0;
     fs_[path] = content;
-
+    printf("path %s, content %s$\n", path.c_str(), content);
     if ((i + 1) < info.number_entry) {
       err = unzGoToNextFile(uf);
       if (err != UNZ_OK) {
@@ -129,77 +138,9 @@ void Zfs::Init(const FunctionCallbackInfo<Value>& args) {
       }
     }
   }
-
-  printf("hello\n");
 }
 
 void Zfs::Read(const FunctionCallbackInfo<Value>& args) {
-  if (!init) {
-    init = true;
-
-    const char* zipfile = "/home/cloud/lib.zip";
-    unzFile uf = unzOpen64(zipfile);
-    if (uf == NULL) {
-      printf("unzip failed\n");
-      return;
-    }
-    unz_global_info64 info;
-    int err = unzGetGlobalInfo64(uf, &info);
-    if (err != UNZ_OK) {
-      printf("unzGetGlobalInfo64 failed\n");
-      return;
-    }
-
-    v8::Isolate* isolate = args.GetIsolate();
-    for (int i = 0; i < info.number_entry; i++) {
-      char filename_inzip[256];
-      unz_file_info64 file_info;
-      err = unzGetCurrentFileInfo64(uf,
-                                    &file_info,
-                                    filename_inzip,
-                                    sizeof(filename_inzip),
-                                    NULL,
-                                    0,
-                                    NULL,
-                                    0);
-      if (err != UNZ_OK) {
-        printf("unzGetCurrentFileInfo64 failed\n");
-        return;
-      }
-
-      printf(" file zip : %s\n", filename_inzip);
-      int len = strlen(filename_inzip);
-      if (filename_inzip[len - 1] == '/') {
-        err = unzGoToNextFile(uf);
-        if (err != UNZ_OK) {
-          printf("error %d with zipfile in unzGoToNextFile\n", err);
-          break;
-        }
-        continue;
-      }
-
-      unzFile file;
-      err = unzOpenCurrentFile(uf);
-      char* content = new char[file_info.uncompressed_size + 1];
-      content[file_info.uncompressed_size] = 0x0;
-      unzReadCurrentFile(uf, content, file_info.uncompressed_size);
-      printf("unzReadCurrentFile 2\n");
-      std::string path(":/");
-      path += filename_inzip;
-
-      printf("path %s\n", path.c_str());
-      content[file_info.uncompressed_size] = 0x0;
-      fs_[path] = content;
-      printf("path %s, content %s$\n", path.c_str(), content);
-      if ((i + 1) < info.number_entry) {
-        err = unzGoToNextFile(uf);
-        if (err != UNZ_OK) {
-          printf("error %d with zipfile in unzGoToNextFile\n", err);
-          break;
-        }
-      }
-    }
-  }
 
   Environment* env = Environment::GetCurrent(args);
   node::Utf8Value filename(env->isolate(), args[0]);
